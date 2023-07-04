c9a49391dd017ea62634a5deb1ee04afe522199e
Ethel2003 <brunicepadonou@gmail.com>
2023-07-03 17:57:00 -0700

    correct task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - write the the lower-case alphabet on a line.
+ * main - print_alphabet- write the lower-case alphabet on a line.
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
